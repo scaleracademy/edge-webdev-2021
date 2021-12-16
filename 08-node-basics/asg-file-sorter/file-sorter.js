c8d78413b1d4b15cee9f4758a3d1247f3b24264f
Arnav Gupta <arnav@codingblocks.com>
2021-12-16 22:05:26 +0530

    assignment:
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ 08-node-basics/asg-file-sorter/file-sorter.js
@@ -0,0 +1,6 @@
+/**
+ * Create a program that does the following - 
+ *  1. Read files 1.txt, 2.txt, 3.txt and all the numbers inside them 
+ *  2. Sort the numbers in ascending order
+ *  3. Write the sorted numbers to a file called sorted.txt
+ */
