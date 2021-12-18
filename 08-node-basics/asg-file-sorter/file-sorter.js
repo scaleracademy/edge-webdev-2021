7c7fc491352fdb8d0bb3df245810c2c53b56c9ad
Abhi-tech-09 <64980572+Abhi-tech-09@users.noreply.github.com>
2021-12-18 19:46:40 +0530

    Update file-sorter.js

--- 08-node-basics/asg-file-sorter/file-sorter.js
+++ 08-node-basics/asg-file-sorter/file-sorter.js
@@ -4,3 +4,57 @@
  *  2. Sort the numbers in ascending order
  *  3. Write the sorted numbers to a file called sorted.txt
  */
+const fs = require("fs")
+
+let arr = []; 
+
+//Reads all the files under current directory 
+fs.readdir(__dirname , (err,files)=>{
+    if(err){
+        console.log("hey");
+        console.log(err);
+    }
+    else{
+        files.forEach(file => {
+            if(file != "file-sorter.js") { 
+                //reads the content and adds into array 
+                fs.readFile(file,'utf-8',(err,data)=>{
+                    if(err)console.log(err);
+                    else{
+                       let str = "" ; 
+                       for(let i = 0 ; i < data.length ; i++){
+                           if(data[i] == "\r"){
+                               if(str.length != "")
+                                arr.push(parseInt(str)); 
+                               str = "" ; 
+                           }
+                           else if(data[i] == "\n") continue ; 
+                           else{
+                               str += data[i] ; 
+                               if(i == data.length-1)arr.push(parseInt(str));
+                           }
+                       }
+                    }
+                })
+            }
+        })
+    }
+})
+
+
+let wait = setInterval(endFun,2000);
+function endFun(){
+    if(arr.length > 0){
+        clearInterval(wait);
+    }
+    arr.sort((a,b)=>{return a-b})
+    console.log(arr);
+    let str = "" ; 
+    for(let i = 0 ; i < arr.length ; i++){
+        str = str + arr[i].toString() + "\n" ; 
+    }
+    // console.log(str);
+    fs.writeFile('sorted.txt',str,(err)=>{
+       if(err) console.log(err);
+    });
+}
