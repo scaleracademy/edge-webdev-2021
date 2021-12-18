91ab157bc0085ef680e1a4af7c377937863ed58f
Arnav Gupta <arnav@codingblocks.com>
2021-12-18 22:03:09 +0530

    get request, and query parameter
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- 09-node-express-server/basic-server/server.js
+++ 09-node-express-server/basic-server/server.js
@@ -1,22 +1,41 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  console.log("HTTP METHOD = " + req.method);
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-    console.log("HTTP METHOD = " + req.method)
-    res.send('Hello World!');
-})
+app.get("/abcd", (req, res) => {
+  res.send({
+      a: 10,
+      b: true,
+      c: "Asdsad",
+      d: {
+          e: "ASdsa"
+      }
+  });
+});
 
-app.get('/abcd', (req, res) => {
-    res.send("WOW! I am abcd");
-})
+app.post("/abcd", (req, res) => {
+  res.send("WOW! You posted to abcd");
+});
 
+app.get("/greet", (req, res) => {
+  console.log(req.query);
+  if (req.query["name"] == undefined) res.send("Hello guest");
+  else res.send("Hello " + req.query["name"]);
+});
 
-app.post('/abcd', (req, res) => {
-    res.send("WOW! You posted to abcd");
+app.get('/arr', (req, res) => {
+    res.send([
+        'asdasd',
+        'asdasd',
+        'sfnsfnsf'
+    ])
 })
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
