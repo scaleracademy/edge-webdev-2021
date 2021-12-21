4ab99dd14dc8d4eac8e74c1d56b36f762fce8092
npawankr1 <58778029+npawankr1@users.noreply.github.com>
2021-12-21 22:40:08 +0530

    completed first task

--- 08-node-basics/asg-file-sorter/file-sorter.js
+++ 08-node-basics/asg-file-sorter/file-sorter.js
@@ -4,3 +4,30 @@
  *  2. Sort the numbers in ascending order
  *  3. Write the sorted numbers to a file called sorted.txt
  */
+const fs = require("fs");
+let sortedData = [];
+const data1 = fs.readFileSync("1.txt", "utf8").split("\n");
+
+const data2 = fs.readFileSync("2.txt", "utf8").split("\n");
+
+const data3 = fs.readFileSync("3.txt", "utf8").split("\n");
+
+for (i in data1) sortedData.push(parseInt(data1[i]));
+for (i in data2) sortedData.push(parseInt(data2[i]));
+for (i in data3) sortedData.push(parseInt(data3[i]));
+
+sortedData.sort((a, b) => {
+  return a - b;
+});
+
+var newData = "";
+for (i = 0; i < sortedData.length; i++){
+  newData += sortedData[i];
+  if(i<sortedData.length-1){
+    newData += "\n";
+  }
+}
+fs.writeFileSync("./sorted.txt", newData, (err) => {
+  if (err) throw err;
+  console.log("sorted data saved");
+});
